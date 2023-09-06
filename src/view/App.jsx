fc9ea0a7fbadfa868a5909589e819f7603b603c0
HaoHanChen <kk12ll55@gmail.com>
2023-09-06 22:51:26 +0800

    feat: add avatar & menu

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    Box,
+    ButtonBase,
+    Flex,
+    Icon,
+    Menu,
+    MenuDivider,
+    MenuGroup,
+    MenuItem,
+    MenuList,
+    MenuToggle,
+    Space,
+    Text,
+    useColorMode,
+    useColorStyle,
+    TonicProvider,
+} from "@tonic-ui/react";
+import FocusLock from "react-focus-lock";
+
+const Avatar = (props) => (
+    <Flex
+        p="1x"
+        borderRadius="50%"
+        alignItems="center"
+        justifyContent="center"
+        fontSize="xs"
+        lineHeight="1"
+        {...props}
+    />
+);
+
+const MyMenu = () => {
+    const [colorMode, setColorMode] = useColorMode();
+    const [colorStyle] = useColorStyle({ colorMode });
+
+    return (
+        <Menu>
+            <MenuToggle>
+                <Avatar
+                    backgroundColor={colorStyle.background.secondary}
+                    color={colorStyle.color.secondary}
+                    _hover={{
+                        color: colorStyle.color.primary,
+                    }}
+                >
+                    <Icon icon="user-team" size="5x" />
+                </Avatar>
+            </MenuToggle>
+            <FocusLock persistentFocus={true}>
+                <MenuList width="max-content">
+                    <Box>
+                        <MenuItem>
+                            <Flex flex="none" mr="3x">
+                                <Icon icon="settings" />
+                            </Flex>
+                            <Flex flex="auto">
+                                <Text>Settings</Text>
+                            </Flex>
+                        </MenuItem>
+                        <MenuItem>
+                            <Flex flex="none" mr="3x">
+                                <Icon icon="user-team" />
+                            </Flex>
+                            <Flex flex="auto">
+                                <Text>Accounts</Text>
+                            </Flex>
+                        </MenuItem>
+                        <MenuItem>
+                            <Flex flex="none" mr="3x">
+                                <Icon icon="lock" />
+                            </Flex>
+                            <Flex flex="auto">
+                                <Text>Privacy control</Text>
+                            </Flex>
+                        </MenuItem>
+                    </Box>
+                </MenuList>
+            </FocusLock>
+        </Menu>
+    );
+};
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <TonicProvider>
+            <MyMenu />
+        </TonicProvider>
+    );
 };
 
 export default App;
